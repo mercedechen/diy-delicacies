9f97441e8b118e067a1b7a0f275a569da0f0f6f3
Mercede Chen <mercedechen@gmail.com>
2024-06-30 01:43:54 -0400

    added front page content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import {GoArrowRight} from "react-icons/go";
 import './App.css'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <div className="container">
+      <div className="book">
+        <h1>DIY DELICACIES</h1>
+        <button id="enter"><GoArrowRight/></button>
+      </div>
+      <div id="annotate">
+        <p>DIY: do-it-yourself</p>
+        <p>Delicacy: (noun) something pleasing to eat that is considered rare or luxurious</p>
+        <p>DIY Delicacies is an application created to share a collection of do-it-yourself recipes that I have tried to make and love. The recipes are either a rendition of or credited to their respective creators.</p>
+      </div>
+    </div>
   )
 }
 
